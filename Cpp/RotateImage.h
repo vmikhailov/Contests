55149feb4be1beb44f3913f1d57cc9dcccc63a27
Viacheslav Mikhailov <viacheslav.mikhailov@gmail.com>
2022-11-08 10:31:01 +0000

    clean up

--- Cpp/RotateImage.h
+++ Cpp/RotateImage.h
@@ -29,34 +29,6 @@ public:
         }
     }
 
-    tuple<int, int> GetSrc(int i, int s) {
-        if (s == 1) return {0, 0};
-        int r = (s - 1) * 4;
-        if (i >= r) {
-            auto [x, y] = GetSrc(i - r, s - 2);
-            return {x + 1, y + 1};
-        } else {
-            if (i < s) return {i, 0};
-            if (i < 2 * s - 1) return {s - 1, i - s + 1};
-            if (i < 3 * s - 2) return {3 * s - i - 3, s - 1};
-            return {0, 4 * s - i - 4};
-        }
-    }
-
-    tuple<int, int> GetDest(int i, int s) {
-        if (s == 1) return {0, 0};
-        int r = (s - 1) * 4;
-        if (i >= r) {
-            auto [x, y] = GetDest(i - r, s - 2);
-            return {x + 1, y + 1};
-        } else {
-            if (i < s) return {s - 1, i};
-            if (i < 2 * s - 1) return {2 * s - i - 2, s - 1};
-            if (i < 3 * s - 2) return {0, 3 * s - i - 3};
-            return {i - 3 * s + 3, 0};
-        }
-    }
-
     void print(vector<vector<int>> &matrix) {
         for (int i = 0; i < matrix.size(); i++) {
             for (int j = 0; j < matrix[i].size(); j++) {
